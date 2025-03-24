38ac223554370dce3e92ea5f4eaf8ede4c82b27a
NicoSantamaria <nicoasantamaria@gmail.com>
2025-03-24 15:38:09 -0700

    express private message modulo public key

--- app/Main.hs
+++ app/Main.hs
@@ -11,14 +11,14 @@ getPrime defaultVal = do
     putStrLn $ "Please choose a prime. Type nothing to use the default value: " ++ show defaultVal
     input <- getLine
     if null input
-        then return defaultVal
+    then return defaultVal
+    else do
+        let userNum = read input :: Integer
+        if isPrime userNum
+        then return userNum
         else do
-            let userNum = read input :: Integer
-            if isPrime userNum
-                then return userNum
-                else do
-                    putStrLn $ input ++ " is not a prime."
-                    getPrime defaultVal
+            putStrLn $ input ++ " is not a prime."
+            getPrime defaultVal
 
 -- get a user-defined public key
 getUserPublicKey :: Integer -> IO Integer
@@ -39,11 +39,10 @@ getUserPrimeFactors firstDefaultPrime secondDefaultPrime e = do
     p <- getUserFactor firstDefaultPrime
     q <- getUserFactor secondDefaultPrime
     if coprime p q e
-        then return (Numbers p q e)
-        else do
-            putStrLn $ "The public key e must be coprime to (p - 1)(q - 1), where p and q are your chosen prime factors. Please choose again."
-            getUserPrimeFactors firstDefaultPrime secondDefaultPrime e
-
+    then return (Numbers p q e)
+    else do
+        putStrLn $ "The public key e must be coprime to (p - 1)(q - 1), where p and q are your chosen prime factors. Please choose again."
+        getUserPrimeFactors firstDefaultPrime secondDefaultPrime e
 
 -- check for divide by zero errors: cant be congruent to the public key mod?
 getMessage :: Integer -> IO Integer
@@ -51,38 +50,37 @@ getMessage defaultVal = do
     putStrLn $ "Please choose a message. Type nothing to use the default value: " ++ show defaultVal
     input <- getLine
     if null input
-        then return defaultVal
-        else return (read input :: Integer)
+    then return defaultVal
+    else return (read input :: Integer)
 
-main :: IO()
+main :: IO ()
 main = do
     putStrLn $ "Welcome to the RSA playground! Here, you can choose numbers..."
-
+    
     -- get e from the user
     putStrLn $ "First, choose a public key. Some strong public keys are Fermat primes, or primes of the form 2^k+1, such as 3, 5, 17, 257, and 65537."
     e <- getUserPublicKey 65537
-    putStrLn $ "You have chosen public key " ++ show e ++ "." 
-
+    putStrLn $ "You have chosen public key " ++ show e ++ "."
+    
     -- get p and q from the user
     putStrLn $ "To encrypt messages, RSA needs two prime numbers to generate the private key. Which prime numbers would you like?"
     numbersIO <- getUserPrimeFactors 7170669219235139 3557745895880441 e
     let (Numbers p q _) = numbersIO
-    putStrLn $ "You have chosen primes " ++ show p ++ " and " ++ show q ++ "."
-
-    -- get m from the user
-    putStrLn $ "Lastly, choose a private message."
-    m <- getMessage 4727576933
-    putStrLn $ "You have chosen private message " ++ show m ++ "." 
-
-    -- Create the numbers and keys
     let numbers = Numbers p q e
     let keys = rsaReceiver numbers
     
-    -- Show the result of encryption and decryption
+    putStrLn $ "You have chosen primes " ++ show p ++ " and " ++ show q ++ "."
+    putStrLn $ "Thus, the public key is " ++ show (getPublicKey numbers) ++ "."
+    putStrLn $ "The private key is " ++ show (getPrivateKey numbers) ++ "."
+    
+    -- get m from the user
+    putStrLn $ "Lastly, choose a private message. The message will be expressed modulo the public key."
+    mIO <- getMessage 4727576933
+    let m = mod mIO e
+    putStrLn $ "You have chosen private message " ++ show m ++ "."
+    
     let encrypted = getPublicMessage keys m
     let decrypted = decode keys encrypted
     
-    -- print results
-    putStrLn $ "Encrypted message: " ++ show encrypted
-    putStrLn $ "Decrypted message: " ++ show decrypted
-
+    putStrLn $ "The encrypted public message is then " ++ show encrypted ++ "."
+    putStrLn $ "After decryption, the received private message is " ++ show decrypted
