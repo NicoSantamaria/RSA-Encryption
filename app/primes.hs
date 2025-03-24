36779ca0052405220258126e724089d2d70f4d29
NicoSantamaria <nicoasantamaria@gmail.com>
2025-03-23 20:48:38 -0700

    make primes a module

--- app/primes.hs
+++ app/primes.hs
@@ -1,12 +1,14 @@
--- checks if a given number is prime (naive approach)
-is_prime :: Integer -> Bool
-is_prime n
-    | n <= 1    = False
-    | n == 2    = True
-    | even n    = False
-    | otherwise = not $ any (divides) [3, 5..integer_sqrt n]
-        where 
-            divides :: Integer -> Bool
-            divides i = mod n i == 0
-            integer_sqrt :: Integer -> Integer
-            integer_sqrt = floor . sqrt . fromIntegral
+module Prime (is_prime) where
+
+    -- checks if a given number is prime (naive approach)
+    is_prime :: Integer -> Bool
+    is_prime n
+        | n <= 1    = False
+        | n == 2    = True
+        | even n    = False
+        | otherwise = not $ any (divides) [3, 5..integer_sqrt n]
+            where 
+                divides :: Integer -> Bool
+                divides i = mod n i == 0
+                integer_sqrt :: Integer -> Integer
+                integer_sqrt = floor . sqrt . fromIntegral
