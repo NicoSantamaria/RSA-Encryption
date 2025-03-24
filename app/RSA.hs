07cd9be0977f906591535bc846f7a686865f8341
NicoSantamaria <nicoasantamaria@gmail.com>
2025-03-24 15:39:34 -0700

    whitespace

--- app/RSA.hs
+++ app/RSA.hs
@@ -6,47 +6,52 @@ module RSA(
     getPublicMessage,
     power,
     decode
-    ) where 
+) where
 
-    -- type for inputting pairs of primes with inverse
-    data Numbers = Numbers {
-        p :: Integer,
-        q :: Integer,
-        e :: Integer
-        } deriving (Show)
+-- type for inputting pairs of primes with inverse
+data Numbers = Numbers {
+    p :: Integer,
+    q :: Integer,
+    e :: Integer
+} deriving (Show)
 
-    -- type for computation of public and private keys
-    data Keys = Keys {
-        publicKey :: (Integer, Integer),
-        privateKey :: (Integer, Integer)
-        } deriving (Show)
+-- type for computation of public and private keys
+data Keys = Keys {
+    publicKey :: (Integer, Integer),
+    privateKey :: (Integer, Integer)
+} deriving (Show)
 
-    -- fast exponential using powers of 2 in a field
-    power :: Integer -> Integer -> Integer -> Integer
-    power b 1 n = mod b n
-    power b y n
-        | even y = mod (power b (div y 2) n ^ 2) n
-        | odd  y = mod (power b (div (y-1) 2) n ^ 2 * b) n
+-- fast exponential using powers of 2 in a field
+power :: Integer -> Integer -> Integer -> Integer
+power b 1 n = mod b n
+power b y n
+    | even y = mod (power b (div y 2) n ^ 2) n
+    | odd y = mod (power b (div (y-1) 2) n ^ 2 * b) n
 
-    -- compute multiplicative inverse with respect to a given modulus
-    inverse :: Integer -> Integer -> Integer
-    inverse 1 b = 1
-    inverse a b = div (r * b + 1) a
-        where r = a - inverse (mod b a) a
+-- compute multiplicative inverse with respect to a given modulus
+inverse :: Integer -> Integer -> Integer
+inverse 1 b = 1
+inverse a b = div (r * b + 1) a
+    where r = a - inverse (mod b a) a
 
-    -- compute the public key
-    getPublicKey :: Numbers -> (Integer, Integer)
-    getPublicKey (Numbers p q e) = (e, p * q)
+-- compute the public key
+getPublicKey :: Numbers -> (Integer, Integer)
+getPublicKey (Numbers p q e) = (e, p * q)
 
-    -- compute the private key
-    getPrivateKey :: Numbers -> (Integer, Integer)
-    getPrivateKey (Numbers p q e) = (inverse e phi, phi)
-        where phi = (p - 1) * (q - 1)
+-- compute the private key
+getPrivateKey :: Numbers -> (Integer, Integer)
+getPrivateKey (Numbers p q e) = (d, n)
+    where 
+        phi = (p - 1) * (q - 1)
+        n = p * q
+        d = inverse e phi
 
-    -- compute the public message from the keys and private message
-    getPublicMessage :: Keys -> Integer -> Integer
-    getPublicMessage (Keys pubKey privKey) privMessage = uncurry (power privMessage) pubKey
+-- compute the public message from the keys and private message
+getPublicMessage :: Keys -> Integer -> Integer
+getPublicMessage (Keys (e, n) _) privMessage = 
+    power privMessage e n
 
-    -- decode the public message to the private message with the public and private keys
-    decode :: Keys -> Integer -> Integer
-    decode (Keys pubKey privKey) pubMessage = power pubMessage (fst privKey) (snd pubKey)
+-- decode the public message to the private message with the public and private keys
+decode :: Keys -> Integer -> Integer
+decode (Keys _ (d, n)) pubMessage = 
+    power pubMessage d n
--- app/primes.hs
+++ app/primes.hs
@@ -1,23 +1,23 @@
 module Primes (isPrime, coprime) where
 
-    -- checks if a given number is prime (naive approach)
-    isPrime :: Integer -> Bool
-    isPrime n
-        | n <= 1    = False
-        | n == 2    = True
-        | even n    = False
-        | otherwise = not $ any (divides) [3, 5..integerSqrt n]
-            where 
-                divides :: Integer -> Bool
-                divides i = mod n i == 0
-                integerSqrt :: Integer -> Integer
-                integerSqrt = floor . sqrt . fromIntegral
-
-    -- determines whether the chosen public key is coprime to phi(pq)
-    coprime :: Integer -> Integer -> Integer -> Bool
-    coprime p q e = gcdEuclid e phi == 1
+-- checks if a given number is prime (naive approach)
+isPrime :: Integer -> Bool
+isPrime n
+    | n <= 1    = False
+    | n == 2    = True
+    | even n    = False
+    | otherwise = not $ any (divides) [3, 5..integerSqrt n]
         where 
-            phi = (p - 1) * (q - 1)
-            gcdEuclid :: Integral a => a -> a -> a
-            gcdEuclid a 0 = a
-            gcdEuclid a b = gcdEuclid b (mod a b)
+            divides :: Integer -> Bool
+            divides i = mod n i == 0
+            integerSqrt :: Integer -> Integer
+            integerSqrt = floor . sqrt . fromIntegral
+
+-- determines whether the chosen public key is coprime to phi(pq)
+coprime :: Integer -> Integer -> Integer -> Bool
+coprime p q e = gcdEuclid e phi == 1
+    where 
+        phi = (p - 1) * (q - 1)
+        gcdEuclid :: Integral a => a -> a -> a
+        gcdEuclid a 0 = a
+        gcdEuclid a b = gcdEuclid b (mod a b)
